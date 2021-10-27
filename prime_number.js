6e969c2e15a0464df4aeaa34278cd524c1104a11
chetannikam <nikamchetan1998@gmail.com>
2021-10-27 11:27:57 +0530

    Prime_number_implementation

---
+++ prime_number.js
@@ -0,0 +1,33 @@
+var start=2;
+var end=100;
+while (start<=end)
+{
+var flag=false;
+if(start==1)
+{
+    console.log(1,"is neighter prime nor composite number")
+}
+if (start==2)
+    {
+        flag=true;
+    }
+for(var i=2;i<start;i++)
+{
+    
+     if(start%i==0)
+    {
+        flag=false;
+        break;
+    }
+    else
+    {
+        flag=true;
+    }
+
+}
+if (flag==true)
+{
+    console.log(start)
+}
+start+=1
+}
